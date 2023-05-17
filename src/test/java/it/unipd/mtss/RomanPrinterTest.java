1870c8afb22cfbed502a20623a5b3b4ea5de1f01
Matteo Umatche <74019874+matteoumatche@users.noreply.github.com>
2023-05-17 08:37:37 +0200

    Update RomanPrinterTest.java

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -21,53 +21,79 @@ public class RomanPrinterTest {
         //Arrange
         // IntegerToRoman integerToRomanMock = Mockito.mock(IntegerToRoman.class);
         // Mockito.when(integerToRomanMock.convert(1)).thenReturn("I");
-        String expectedOutput=" |_   _|\n | |  \n | |  \n | |  \n |_____|";
+        String expectedOutput=
+            "|_   _|\n" + 
+            "  | |  \n" +
+            "  | |  \n" +
+            "  | |  \n" +
+            "|_____|\n";
         //Act
         String actualOutput=RomanPrinter.print(1);
         //Assert
         assertEquals(expectedOutput, actualOutput);
     }
-    /*
-    @Ignore
+    
+    
     @Test
     public void PrintNumberFive_AsciiArt_V(){
         //Arrange
-        String expectedOutput="";
+        String expectedOutput=
+            "\\ \\    / /\n"+
+            " \\ \\  / / \n"+
+            "  \\ \\/ /  \n"+
+            "   \\  /    \n"+
+            "    \\/     \n";
         //Act
-        String actualOutput=RomanPrinter.print();
+        String actualOutput=RomanPrinter.print(5);
         //Assert
         assertEquals(expectedOutput, actualOutput);
     }
-    @Ignore
+    
     @Test
     public void PrintNumberTen_AsciiArt_X(){
         //Arrange
-        String expectedOutput="";
+        String expectedOutput=
+            " \\ \\ / / \n"+
+            "  \\ V /  \n"+
+            "   > <   \n"+
+            "  / . \\  \n"+
+            " /_/ \_\\ \n";
         //Act
-        String actualOutput=RomanPrinter.print();
+        String actualOutput=RomanPrinter.print(10);
         //Assert
         assertEquals(expectedOutput, actualOutput);
     }
-    @Ignore
+    
     @Test
-    public void PrintNumberFifty_AsciiArt_L(){
+    public void PrintNumberFifty_AsciiArt_L(50){
         //Arrange
-        String expectedOutput="";
+        String expectedOutput=
+            " | |     \n"+
+            " | |     \n"+
+            " | |     \n"+
+            " | |     \n"+
+            " |______|\n";
         //Act
         String actualOutput=RomanPrinter.print();
         //Assert
         assertEquals(expectedOutput, actualOutput);
     }
-    @Ignore
+    
     @Test
     public void PrintNumberOneHundred_AsciiArt_C(){
         //Arrange
-        String expectedOutput="";
+        String expectedOutput=
+            "  / ____|\n"+
+            " | |     \n"+
+            " | |     \n"+
+            " | |     \n"+
+            "  \\_____|\n";
         //Act
-        String actualOutput=RomanPrinter.print();
+        String actualOutput=RomanPrinter.print(100);
         //Assert
         assertEquals(expectedOutput, actualOutput);
     }
+    /*
     @Ignore
     @Test
     public void PrintNumberEightyNine_AsciiArt_IXC(){
